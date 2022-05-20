d4e1d5e642640b4fc6068128e5fb5de90cad1344
Sheldon <nkeyskou124@gmail.com>
2022-05-20 11:32:37 +0700

    login and join room

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -24,6 +24,7 @@ navigator.mediaDevices.getUserMedia({
 
     socket.on('user-connected', userId => {
         connectToNewUser(userId, stream);
+        console.log(userId);
     }) 
 })
 
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,30 @@ var router = express.Router();
 const { v4:uuidV4 } = require('uuid');
 
 /* GET home page. */
+router.get('/join', (req, res) => {
+  res.render('join');
+})
+
+router.post('/join', (req, res) => {
+  var username = req.body.username;
+  var roomId = req.body.roomId;
+
+  req.session.user = username;
+  req.session.roomId = roomId;
+
+  res.redirect('/');
+});
+
 router.get('/', function(req, res, next) {
-  res.redirect(`${ uuidV4() }`);
+  if(!req.session.user) {
+    return res.redirect('/join');
+  }
+
+  return res.redirect(`/${req.session.roomId}`);
 });
 
 router.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room });
+  res.render('room', {username: req.session.user, roomId: req.params.room });
 })
 
 module.exports = router;
